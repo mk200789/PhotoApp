245f4e980fb6ecea10ca1cf0dd4d43ace41aa55f
WANKIM MOK <cafe.mui@gmail.com>
2014-05-15 19:03:45 -0400

    remove unnecessary code

--- js/app/photoctrl/photoctrl.js
+++ js/app/photoctrl/photoctrl.js
@@ -4,19 +4,23 @@ var app = angular.module('PhotoAppControllers', []);
 
 /*Controllers*/
 
-app.controller("PhotoCtrl", function($scope, $http, $routeParams) {
-	$http.get('js/photo.json').success(function(data){
-		$scope.pictures = data;
-		$scope.kitty="this is kitty";
-		//$scope.pictures = data.splice(0,3); display the first 3 images		
-	});
-	$scope.expandImg = false;
-	$scope.picturesId= $routeParams.picturesId;
+app.controller("PhotoCtrl", function($rootScope, $http, $routeParams) {
 
-	$scope.keepTrac = function(input)
+	
+	$http.get('js/photo.json').success(function(data){
+		$rootScope.pictures = data;
+		//$scope.pictures = data.splice(0,3); display the first 3 images
+
+	});
+
+
+	$rootScope.picturesId= $routeParams.picturesId;
+
+	$rootScope.keepTrac = function(input)
 	{
-		$scope.tell = $scope.pictures[input-1].url;
-	}
+		$rootScope.tell = $rootScope.pictures[input-1].url;
+	};
+
 
 
 });
