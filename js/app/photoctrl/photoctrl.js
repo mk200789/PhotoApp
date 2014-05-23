820e61df96282481384b4b4eacad8e8a4601e88c
WANKIM MOK <cafe.mui@gmail.com>
2014-05-23 01:01:07 -0400

    create custom search filter, category list base on available categories, and a search and submit function

--- js/app/photoctrl/photoctrl.js
+++ js/app/photoctrl/photoctrl.js
@@ -4,30 +4,97 @@ var app = angular.module('PhotoAppControllers', []);
 
 /*Controllers*/
 
-app.controller("PhotoCtrl", function($rootScope, $http, $routeParams) {
+app.controller("PhotoCtrl", function($scope, $http, $routeParams, $location) {
 
-	
-	$http.get('js/photo.json').success(function(data){
-		$rootScope.pictures = data;
-		//$scope.pictures = data.splice(0,3); display the first 3 images
+	//keeps track of whether if something is submitted or entered
+	$scope.isSubmit = false;
 
-	});
+	/*FILTERING CATEGORIES*/
+	$scope.availableCategory= [];
+
+	$scope.catFilter = null;
+
+	$scope.init = function(){
+		$http.get('js/photo.json').success(function(data){
+			//console.log(data);
+
+			$scope.pictures = data;
+
+			angular.forEach(data, function(value,index){
+
+				var exist = false;
+				
+				angular.forEach($scope.availableCategory, function(cat, index){
+					if(value.category === cat)
+					{
+						exist = true;
+					}
+				});
+				if(exist ===false){
+				$scope.availableCategory.push(value.category);}				
+			});
+		});
+
+		$scope.keepTrac = function(input)
+		{
+			$scope.tell = $scope.pictures[input-1].url;
+		};
+
+		$scope.grab = function(input)
+		{
+			$scope.passVal = input;
+		};
+
+		$scope.go = function(input)
+		{
+			console.log(input);
+			$location.path(input);
+
+		};
 
 
-	$rootScope.picturesId= $routeParams.picturesId;
-
-	$rootScope.keepTrac = function(input)
-	{
-		$rootScope.tell = $rootScope.pictures[input-1].url;
 	};
+	/* END OF FILTERING CATEGORIES*/	
 
 
+	$scope.picturesId= $routeParams.picturesId;
 
+
+	$scope.search = function(input){
+		//check if something is submitted
+		if($scope.isSubmit){
+			$scope.searchResult=null;
+			//check if submitted is not blank/empty
+			if(input){
+				$scope.searchResult = input;
+				console.log($scope.searchResult);
+			}
+			//reset isSubmit to false
+
+			$scope.isSubmit= false;
+		}
+	};	
+
+	$scope.submit = function(){
+			$scope.isSubmit = true;
+	};
 });
 
-/*app.controller('PhotoDetailCtrl',
-	function($scope, $routeParams){
-		$scope.photoId = $routeParams.photoId;
 
-	});
-*/
+app.filter('isCategory',  function() {
+    return function(input, category) {
+       // console.log("input" + input);
+        if (typeof category == 'undefined' || category == null) {
+            return input;
+        } else {
+            var out = [];
+            for (var a = 0; a < input.length; a++){
+                    if(input[a].category == category) {
+                        out.push(input[a]);
+                    }
+
+            }
+            return out;
+        }
+    };
+});
